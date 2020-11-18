18b01df5dc421ffe42e7237369e74fbd829fa5d0
dv1veidis <limessi61@gmail.com>
2020-11-19 00:14:07 +0200

    WebApp

---
+++ WebSocket/src/main/java/lt/GamePals/WebSocket/Controler/ChatController.java
@@ -0,0 +1,34 @@
+package lt.GamePals.WebSocket.Controler;
+
+import org.springframework.stereotype.Controller;
+import lt.GamePals.WebSocket.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+
+
+@Controller
+public class ChatController 
+{
+	
+	@MessageMapping ("/chat.send")
+	@SendTo("/topic/public")
+	public ChatMessage sendMessage (@Payload final ChatMessage chatMessage)
+	{
+		return chatMessage;
+	}
+	
+	@MessageMapping ("/chat.newUser")
+	@SendTo("/topic/public")
+	public ChatMessage newUser(@Payload final ChatMessage chatMessage,
+								SimpMessageHeaderAccessor headerAccessor)
+	{
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		return chatMessage;
+	}
+		
+
+	
+	
+}
---
+++ WebSocket/src/main/java/lt/GamePals/WebSocket/model/MessageType.java
@@ -0,0 +1,9 @@
+package lt.GamePals.WebSocket.model;
+
+public enum MessageType {
+
+	CHAT,
+	CONNECT,
+	DISCONNECT
+}
+
